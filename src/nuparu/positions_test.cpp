9199da37e15de2cd37ef89014208787ad6dfae6d
shivvo <shivr2035@gmail.com>
2024-02-20 18:09:07 -0500

    Add positions test template

---
+++ src/nuparu/positions_test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#define BOOST_TEST_MODULE nuparu_example_test
+
+#include <boost/dynamic_bitset.hpp>
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(ExampleTestSuite)
+
+// Checks that bit swap can be extracted from encoding
+BOOST_AUTO_TEST_CASE(EXAMPLE_TEST) { std::cout << "hello, world!"; }
+
+BOOST_AUTO_TEST_SUITE_END()
